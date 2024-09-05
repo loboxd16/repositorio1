9636e59bdc6313c56a81d741d2b3361767047ec6
unknown <dpaezmantari@gmail.com>
2024-09-04 20:36:46 -0500

    programitas

--- Program.cs
+++ Program.cs
@@ -2,3 +2,11 @@
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("hola webones!");
+
+long edad=20;
+double dinero=3423;
+float=1243.12;
+string nombre="YONI";
+char letra ='ASA';
+bool tienegorro=false;
+
