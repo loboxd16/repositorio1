c28b6f6f089960cc7c756e776fecde7f3baf6b60
unknown <dpaezmantari@gmail.com>
2024-09-04 22:30:12 -0500

    programitas2

--- Program.cs
+++ Program.cs
@@ -1,12 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-Console.WriteLine("hola webones!");
+int opcion = 0;
+while (opcion != 7) { 
+Console.WriteLine("Ingresa los numeros a operar");
+int numero1=int.Parse(Console.ReadLine());
+int numero2=int.Parse(Console.ReadLine());
+int resultado = 0;
 
-long edad=20;
-double dinero=3423;
-float=1243.12;
-string nombre="YONI";
-char letra ='ASA';
-bool tienegorro=false;
-
+Console.WriteLine("1 sumar");
+Console.WriteLine("1 restar");
+Console.WriteLine("1 multiplicar");
+Console.WriteLine("1 dividir");
+Console.WriteLine("1 potencia");
+Console.WriteLine("1 raiz");
+Console.WriteLine("1 salir");
+Console.WriteLine("ingresa opcion");
+opcion=int.Parse(Console.ReadLine());
+switch (opcion)
+{
+    case1:
+    resultado = numero2 + numero1;
+    break;
+case 2:
+resultado = numero2 + numero1;
+        break;
+    case 2:
+resultado = numero2 + numero1;
+        break;
+    case 3:
+resultado = numero2 + numero1;
+        break;
+    case 4:
+resultado = numero2 + numero1;
+        break;
+    default:
+break;
+    
+}
+Console.WriteLine("resultado es{Math.Round(resultado, 2)";
+    }
+    Console.WriteLine("Salida");
---
+++ Program2.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("hola webones!");
+
+long edad=20;
+double dinero=3423;
+float=1243.12;
+string nombre="YONI";
+char letra ='ASA';
+bool tienegorro=false;
+
